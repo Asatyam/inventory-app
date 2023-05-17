b49b117ee4c136f468caa15db7e91fcaf97b689b
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-05-17 21:00:18 +0530

    Add the item_create_post controller

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -26,4 +26,54 @@ exports.item_create_get = asyncHandler(async(req,res,next)=>{
         title: "Add item",
         categories:categories,
     });
-})
+});
+
+exports.item_create_post = [
+
+
+  body('name', 'Name must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('category', 'Category must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('description', 'Description must not be empty.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body('price', 'Price must not be empty').trim().escape(),
+  body('available').escape(),
+  // Process request after validation and sanitization.
+
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a Book object with escaped and trimmed data.
+    const item = new Item({
+      name: req.body.name,
+      category: req.body.category,
+      description: req.body.description,
+      price: req.body.price,
+      available: req.body.available,
+    });
+
+    if (!errors.isEmpty()) {
+     
+     const categories = await Category.find().exec();
+
+      res.render('item_form', {
+        title: 'Add item',
+        categories: categories,
+        item: item,
+        errors: errors.array(),
+      });
+    } else {
+      // Data from form is valid. Save book.
+      await item.save();
+      res.redirect(item.url);
+    }
+  }),
+];
--- routes/catalog.js
+++ routes/catalog.js
@@ -13,7 +13,7 @@ router.post('/category/:id/update', category_controller.category_update_post);
 router.get('/category/:id', category_controller.category_detail);
 
 router.get('/item/create',item_controller.item_create_get);
-// router.post('/item/create',item_controller.item_create_post);
+router.post('/item/create',item_controller.item_create_post);
 // router.get('/item/:id/delete',item_controller.item_delete_get);
 // router.post('/item/:id/delete',item_controller.item_delete_post);
 // router.get('/item/:id/update', item_controller.item_update_get);
