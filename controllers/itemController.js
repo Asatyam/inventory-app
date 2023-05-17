412644b8ed7e53f1f7184f9d04e3bbf71b03e121
SatyamAgrawal <95954551+Asatyam@users.noreply.github.com>
2023-05-17 20:22:05 +0530

    Implement function item_detail

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -3,3 +3,19 @@ const Item = require('../models/item');
 const asyncHandler = require('express-async-handler');
 const { body, validationResult } = require('express-validator');
 
+exports.item_detail = asyncHandler(async(req,res,next)=>{
+
+    const item = await Item.findById(req.params.id).populate("category").exec();
+
+    if(item === null){
+        const err = new Error("Item not found");
+        err.status = 404;
+        return next(err);
+    }
+
+    res.render("item_detail",{
+        title: item.name,
+        item: item,
+    });
+
+})
